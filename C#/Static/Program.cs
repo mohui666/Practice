1aff8888c7bde29466cf146980a7b05a2fd6aeeb
mohui666 <mmmmmohui@gmail.com>
2022-12-22 10:47:58 +0800

    1

---
+++ C#/Static/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test a = new Test();
+            Test b = new Test();
+            a.hp = 10;
+            Test.speed = 1000;
+
+            Console.WriteLine(Test.speed);
+            Console.WriteLine(a.hp);
+        }
+    }
+}
---
+++ C#/Static/Test.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Static
+{
+    class Test
+    {
+        public int hp = 10;
+        public static int speed = 100;
+    }
+}
